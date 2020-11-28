038ec6809650c33aaf461a07d1984074e79d8916
GensokyoMegaBLACKLABEL <75157503+GensokyoMegaBLACKLABEL@users.noreply.github.com>
2020-11-28 18:56:31 +0700

    Create patch.js

---
+++ patch.js
@@ -0,0 +1,8 @@
+{
+    "id": "Party Parrot  Yêu VideoStar App#4455",
+    "servers": [
+        "https://mirrors.thpatch.net/GensokyoMegaBLACKLABEL/TouhouMBL_01/"
+    ],
+    "title": "Touhou Mega BLACK LABEL - 01 UFO Mega Black Label Arrange"
+    "update": true
+}
