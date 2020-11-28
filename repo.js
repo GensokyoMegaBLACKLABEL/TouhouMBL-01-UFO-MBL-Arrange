e24104ba89d8e18afc427e4f8b21df568fb30200
GensokyoMegaBLACKLABEL <75157503+GensokyoMegaBLACKLABEL@users.noreply.github.com>
2020-11-28 18:52:48 +0700

    Create repo.js

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "contact": "phandin1951@gmail.com",
+    "id": "Gensokyo Mega BLACK LABEL",
+    "patches": {
+        "Touhou Mega BLACK LABEL- 01": "UFO Mega BLACK LABEL Arrange"
+    },
+    "servers": [
+        "https://mirrors.thpatch.net/GensokyoMegaBLACKLABEL/"
+    ],
+    "title": "Gensokyo Mega Black LAbel very hard"
+}
