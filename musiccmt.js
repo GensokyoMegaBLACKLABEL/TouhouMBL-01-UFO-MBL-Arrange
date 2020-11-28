ec642abb735c412d270926610af9d6cfe83ef7bd
GensokyoMegaBLACKLABEL <75157503+GensokyoMegaBLACKLABEL@users.noreply.github.com>
2020-11-28 19:28:16 +0700

    Add files via upload

---
+++ musiccmt.js
@@ -0,0 +1,172 @@
+{
+    "1": [
+        "@",
+        "　",
+        "This is the title screen theme.",
+        "",
+        "Well, it's the same feeling as always.",
+        "I tried to give it a particularly affected air.",
+        "I want to look up at the sky while I'm walking, but I'm afraid people",
+        "will think I'm weird, so I look straight ahead when I walk."
+    ],
+    "2": [
+        "@",
+        "　",
+        "The stage 1 theme.",
+        "",
+        "I wanted to start with something nice and casual, so this is how it came out.",
+        "I guess bright songs just aren't that great for shooting games?",
+        "Or so say people whose likes and tastes change at the time with their moods.",
+        "It's so invigorating that it makes me want to fly out the window."
+    ],
+    "3": [
+        "@",
+        "　",
+        "This is Nazrin's theme.",
+        "",
+        "Isn't the song boss-like? Never mind how she looks.",
+        "Once, a long time ago, a mouse climbed into the kotatsu with me.",
+        "It even bit me. The bleeding wouldn't stop, so I had to stay home from school.",
+        "Mice are actually pretty strong, so they're dangerous. Never mind how they look."
+    ],
+    "4": [
+        "@",
+        "　",
+        "This is the stage 2 theme.",
+        "",
+        "I tried to bring out a floating feeling the stage 1 theme didn't have.",
+        "You're swimming through the clouds, where surely there are youkai.",
+        "One imagines it must feel good, but it's probably real cold.",
+        "Your clothes would probably freeze. I wonder if I'd have the guts to do it."
+    ],
+    "5": [
+        "@",
+        "　",
+        "This is Kogasa Tatara's theme.",
+        "",
+        "Most people probably buy quite a few umbrellas over their lives.",
+        "You forget them on trains, and you buy vinyl umbrellas when you're caught in sudden rain.",
+        "Anyway, I wrote this song based on the image of a self-centered karakasa youkai",
+        "who gets depressed easily despite her cheery nature."
+    ],
+    "6": [
+        "@",
+        "　",
+        "This is the stage 3 theme.",
+        "",
+        "I tried to evoke a cryptic atmosphere without losing the airiness.",
+        "When it comes to floating in the sky, you really do think of Laputa;",
+        "but I've already made that descent.",
+        "So, when it comes to ruins... you still think of Laputa."
+    ],
+    "7": [
+        "@",
+        "　",
+        "This is Ichirin Kumoi & Unzan's theme.",
+        "",
+        "Since they're pretty fun characters, I tried to make it fun and old-fashioned.",
+        "It's really nice, sometimes silly and sometimes sentimental.",
+        "Unzan is made out of clouds, so don't worry about it if more than two",
+        "of him or four of his arms show up at once. Flash of insight!"
+    ],
+    "8": [
+        "@",
+        "　",
+        "This is the stage 4 theme.",
+        "",
+        "Aren't journeys great? I feel the most excited when I'm riding on a vessel.",
+        "Even if it's the ship of the infamous ghost captain who sinks lots of other ships.",
+        "I mixed anticipation and anxiety together as much as I could when making this song,",
+        "trying to give it a sense of excitement."
+    ],
+    "9": [
+        "@",
+        "　",
+        "This is Minamitsu Murasa's theme.",
+        "",
+        "Since she sinks lots of ships, she's a well-known ghost captain.",
+        "I tried to give it a \"captain\" atmosphere, so it's a little too cool.",
+        "Everyone calls her Murasa, but \"murasa\" is the name of a youkai,",
+        "so they're not really calling her by name - surprise!"
+    ],
+    "10": [
+        "@",
+        "　",
+        "This is the stage 5 theme.",
+        "",
+        "This is a song with irregular rhythms, but it's upbeat. Pretty Makai-ish.",
+        "It gets pretty intense when you're listening to it with the sound effects from gameplay,",
+        "so I hope you'll listen closely while playing.",
+        "The name comes from the song having both a rural and an urban feel."
+    ],
+    "11": [
+        "@",
+        "　",
+        "This is Shou Toramaru's theme.",
+        "",
+        "I gave this song a quiet, but strong feel. The second half is awfully boss-ish.",
+        "It's a feeling of dignity rather than terror.",
+        "Still, those lasers coming one after the other are pretty scary.",
+        "They're old-man killers you need really good reflexes to dodge."
+    ],
+    "12": [
+        "@",
+        "　",
+        "This is the stage 6 theme.",
+        "",
+        "A song with too much tension.",
+        "The stage is short and not too hard, so I had to add to the tension.",
+        "",
+        "The heartbeats make you want to hurry, don't they?"
+    ],
+    "13": [
+        "@",
+        "　",
+        "This is Byakuren Hijiri's theme.",
+        "",
+        "From the quiet heartbeat, suddenly it feels like the world's opening up.",
+        "This is the theme of a last boss who's hard to tire out, probably.",
+        "It's overall fairly melodiously Japanese, and I tried to make the melody of the",
+        "main part like one of those songs where everyone's singing. Human, bare your feelings and GO!"
+    ],
+    "14": [
+        "@",
+        "　",
+        "This is the extra stage theme.",
+        "",
+        "Aren't UFOs cute?",
+        "The extra is much different than the main stages, and this song is a cute send-off.",
+        "I went for a pop feel for this song.",
+        "UFOs are kind of pop-ish, after all."
+    ],
+    "15": [
+        "@",
+        "　",
+        "This is Nue Houjuu's theme.",
+        "",
+        "A vigorously youkai youkai song.",
+        "It's a song with an old-fashioned, Japanese sense of terror and sorrow floating about.",
+        "Isn't it really fitting for an EX boss?",
+        "The mostly one-shot recording full of ad-libbing is also pretty EX-ish."
+    ],
+    "16": [
+        "@",
+        "　",
+        "This is the ending theme.",
+        "",
+        "Usually the ending is for a Shinto shrine, but this time it's for a Buddhist temple.",
+        "",
+        "Temples have a much darker image than shrines.",
+        "Well, calling it \"dark\" is kind of rude. I meant \"boring\" (just kidding)"
+    ],
+    "17": [
+        "@",
+        "　",
+        "This is the staff roll theme.",
+        "",
+        "It ended up being an arrangement of Emotional Skyscraper.",
+        "",
+        "This song has the feel of the quiet trip home after a journey.",
+        "Oh, look up there~, there's a UFO in the night sky."
+    ]
+}
