f46eb3e30892b0da078ad8efc5d20768b6709f4b
GensokyoMegaBLACKLABEL <75157503+GensokyoMegaBLACKLABEL@users.noreply.github.com>
2020-11-28 19:03:45 +0700

    Create en.js

---
+++ en.js
@@ -0,0 +1,24 @@
+{
+  "console": false,
+  "dat_dump": false,
+  "patches": [
+    {
+      "archive": "nmlgc/base_tsa/"
+    },
+    {
+      "archive": "nmlgc/base_tasofro/"
+    },
+    {
+      "archive": "nmlgc/script_latin/"
+    },
+    {
+      "archive": "nmlgc/western_name_order/"
+    },
+    {
+      "archive": "thpatch/lang_en/"
+    },
+    {
+      "archive": "GensokyoMegaBLACKLABEL/Touhou Mega Black Label- 01/"
+    }
+  ]
+}
